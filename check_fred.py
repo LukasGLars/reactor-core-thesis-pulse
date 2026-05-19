bbf2a8d01c08750a0c67f6478b562b16295380da
LukasGLars <lukasglarsson88@gmail.com>
2026-05-19 12:27:17 +0200

    tmp: remove check script

--- check_fred.py
+++
@@ -1,6 +0,0 @@
-import requests, os
-key = os.environ.get("FRED_API_KEY","")
-for sid in ["DBAA","DAAA"]:
-    url = "https://api.stlouisfed.org/fred/series?series_id={}&api_key={}&file_type=json".format(sid, key)
-    s = requests.get(url, timeout=10).json().get("seriess",[{}])[0]
-    print("{}: start={} freq={}".format(sid, s.get("observation_start"), s.get("frequency_short")))
