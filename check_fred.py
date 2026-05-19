3793fb05dd796a1c430b8a0ab4dd18c7dc8278dc
LukasGLars <lukasglarsson88@gmail.com>
2026-05-19 12:26:33 +0200

    tmp: check DBAA DAAA

--- check_fred.py
+++ check_fred.py
@@ -1,8 +1,6 @@
 import requests, os
 key = os.environ.get("FRED_API_KEY","")
-for sid in ["BAMLH0A0HYM2","BAMLC0A0CM","BAA"]:
+for sid in ["DBAA","DAAA"]:
     url = "https://api.stlouisfed.org/fred/series?series_id={}&api_key={}&file_type=json".format(sid, key)
     s = requests.get(url, timeout=10).json().get("seriess",[{}])[0]
-    start = s.get("observation_start")
-    freq  = s.get("frequency_short")
-    print("{}: start={} freq={}".format(sid, start, freq))
+    print("{}: start={} freq={}".format(sid, s.get("observation_start"), s.get("frequency_short")))
