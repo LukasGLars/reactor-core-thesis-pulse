1e26aab4f4d38cd12428bd5759005ab368e791aa
LukasGLars <lukasglarsson88@gmail.com>
2026-05-19 12:25:16 +0200

    tmp: fix syntax

--- check_fred.py
+++ check_fred.py
@@ -1,6 +1,8 @@
-import requests, os, json
+import requests, os
 key = os.environ.get("FRED_API_KEY","")
 for sid in ["BAMLH0A0HYM2","BAMLC0A0CM","BAA"]:
-    url = f"https://api.stlouisfed.org/fred/series?series_id={sid}&api_key={key}&file_type=json"
-    d = requests.get(url,timeout=10).json().get("seriess",[{}])[0]
-    print(f"{sid}: start={d.get(\"observation_start\")} freq={d.get(\"frequency_short\")}")
+    url = "https://api.stlouisfed.org/fred/series?series_id={}&api_key={}&file_type=json".format(sid, key)
+    s = requests.get(url, timeout=10).json().get("seriess",[{}])[0]
+    start = s.get("observation_start")
+    freq  = s.get("frequency_short")
+    print("{}: start={} freq={}".format(sid, start, freq))
